1b88bf1a11ee84b59e39ce1240631f224da3d2ce
juda-star <juda.ishta@gmail.com>
2022-12-13 09:26:21 +0200

    deploy

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import Footer from "../../features/Footer/Footer";
 import Form from "../../features/Form/Form";
 import HeroProject from "../../features/HeroProject/HeroProject";
 import Navbar from "../../features/Navbar/Navbar";
-
+import "./contact.css";
 const Contact = () => {
   return (
     <div>
